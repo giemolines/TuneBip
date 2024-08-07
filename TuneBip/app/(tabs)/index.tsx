282df221767ae2f9f9aeeeb9a30418a746cc2780
cam_sanjuan <135734842+giemolines@users.noreply.github.com>
2024-08-06 22:38:43 -0600

    firebase auth finished

--- TuneBip/app/(tabs)/index.tsx
+++ TuneBip/app/(tabs)/index.tsx
@@ -1,31 +1,29 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { auth } from '@/FirebaseConfig';
+import { getAuth } from 'firebase/auth';
+import { router } from 'expo-router';
 
 export default function TabOneScreen() {
+  getAuth().onAuthStateChanged((user) => {
+    if (!user) router.replace('/');
+  });
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>SignOut</Text>
+      <TouchableOpacity onPress={()=> auth.signOut()}>
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  container:{
+    backgroundColor:'white',
+    flex:1,
+  }
+  
 });
--- TuneBip/app/index.tsx
+++ TuneBip/app/index.tsx
@@ -1,52 +1,151 @@
-import {View, Text, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Button} from  'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { auth } from '../FirebaseConfig'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
-import { router } from 'expo-router'
+import React, { useState } from 'react';
+import { View, Text, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { auth } from '../FirebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { router } from 'expo-router';
 
-const index  = () => {
-    //STATE HOOKS FOR EMAIL AND PASSWORD
+export default function Index() {
+    // STATE HOOKS FOR EMAIL AND PASSWORD
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    //SIGN IN AND SIGNUP FUNCTIONS
+    // SIGN IN AND SIGNUP FUNCTIONS
     const signIn = async () => {
-        try{
-            const user = await signInWithEmailAndPassword(auth, email, password)
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password);
             if (user) router.replace('/(tabs)');
-        } catch (error: any){
-            console.log(error)
+        } catch (error: any) {
+            console.log(error);
             alert('Sign in failed: ' + error.message);
         }
-    }
+    };
+
     const signUp = async () => {
-        try{
-            const user = await createUserWithEmailAndPassword(auth, email, password)
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, password);
             if (user) router.replace('/(tabs)');
-        } catch (error: any){
-            console.log(error)
-            alert('Sign in failed: ' + error.message);
+        } catch (error: any) {
+            console.log(error);
+            alert('Sign up failed: ' + error.message);
         }
-    }
+    };
 
-    return(
-        <SafeAreaView>
-            <KeyboardAvoidingView>
-                <Text>Login</Text>
-                <TextInput placeholder='Email' value={email} onChangeText={setEmail}/>
-                <TextInput placeholder='Password' value={password} onChangeText={setPassword}/>
-                <TouchableOpacity onPress={signIn}>
-                    <Button title='Login'/>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={signIn}>
-                    <Button title='Register'/>
-                </TouchableOpacity>
+    return (
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.innerCointainer}>
+                    <Text style={styles.headerText}>TuneBip Login</Text>
+                    <Image source={require('../assets/images/tunebip_logo.png')} style={styles.logo}/>
+                    <Text style={styles.h2}>Create an account</Text>
+                    <Text style={styles.h3}>Enter your email to sign up for TuneBip</Text>
+                    <TextInput style={styles.input} placeholder='email@domain.com' value={email}  onChangeText={setEmail} keyboardType='email-address'/>
+                    <TextInput style={styles.input} placeholder='Password' value={password} onChangeText={setPassword} secureTextEntry={true}/>
+                    <TouchableOpacity onPress={signIn} style={styles.loginButton}>
+                        <Text style={styles.buttonText}>Login</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={signUp} style={styles.signUpButton}>
+                        <Text style={styles.buttonText}>Sign up with email</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.policy}>By clicking continue, you agree to our <Text style={styles.bold}>Terms of Service</Text> and <Text style={styles.bold}>Privacy Policy</Text></Text>
             </KeyboardAvoidingView>
-
         </SafeAreaView>
-        
-    )
+    );
 }
 
-export default index
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: 'column',
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+
+    innerCointainer:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        padding: 20,
+    },
+
+    headerText:{
+        fontSize:35,
+        fontWeight:'600',
+        color: '#000',
+    },
+
+    logo:{
+        resizeMode: 'contain',
+        width:200,
+        height:100,
+        marginTop: 40,
+        marginBottom:40,
+    },
+
+    h2:{
+        fontSize:23,
+        fontWeight:'600',
+        marginBottom: 10,
+    },
+
+    h3:{
+        fontSize:20,
+        marginBottom:30
+    },
+
+    input:{
+        height:50,
+        width: '100%',
+        borderRadius: 15,
+        borderColor: 'lightgray',
+        borderWidth: 1,
+        marginBottom: 20,
+        paddingHorizontal: 10
+
+    },
+
+    loginButton:{
+        justifyContent: "center",
+        alignItems: 'center',
+        width: '100%',
+        height: 50,
+        borderRadius: 15,
+        backgroundColor: 'black',
+        marginBottom: 10,
+        paddingHorizontal: 10
+    },
+    signUpButton:{
+        justifyContent: "center",
+        alignItems: 'center',
+        width: '100%',
+        height: 50,
+        borderRadius: 15,
+        backgroundColor: 'indigo',
+        marginBottom: 10,
+        paddingHorizontal: 10
+    },
+    buttonText:{
+        fontSize:20,
+        fontWeight:'500',
+        textAlign: 'center',
+        color: 'white',
+    },
+
+    policy:{
+        marginTop:80,
+        padding:20,
+        fontSize:16,
+        color: 'grey',
+    },
+
+    bold:{
+        color: 'black',
+        fontWeight:'medium'
+
+    }
+
+
+
+    
+});
