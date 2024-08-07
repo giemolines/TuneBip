33f22ae804c57c2bfbaad6285ac89ccb9a214acf
cam_sanjuan <135734842+giemolines@users.noreply.github.com>
2024-08-07 09:47:44 -0600

    update

--- TuneBip/app/(tabs)/index.tsx
+++ TuneBip/app/(tabs)/index.tsx
@@ -101,7 +101,7 @@ const styles = StyleSheet.create({
     shadowColor: 'indigo',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.5,
-    shadowRadius: 15,
+    shadowRadius: 10,
   },
 
   profile:{
